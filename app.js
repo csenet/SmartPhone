21d82dcb3c9e56096d701e2a187e2a55f644f6ac
kouichihirachi <kouichi.hirachi@gmail.com>
2020-08-22 20:52:17 +0900

    Test

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-var express = require('express');
-var app = express();
-var expressWs = require('express-ws')(app);
+var fs = require('fs');
+var port = process.env.PORT || 8000;
+var server = require('http').createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var output = fs.readFileSync('./index.html', 'utf-8');
+    res.end(output);
+}).listen(port);
+var io = require('socket.io').listen(server);
 
-app.use(function (req, res, next) {
-    console.log('middleware');
-    req.testing = 'testing';
-    return next();
-});
+var userHash = {};
 
-app.get('/', function (req, res, next) {
-    console.log('get route', req.testing);
-    res.end();
-});
+io.sockets.on('connection', function (socket) {
 
-app.ws('/', function (ws, req) {
-    ws.on('message', function (msg) {
-        console.log(msg);
+    socket.on('connected', function (name) {
+        var msg = name + 'が入室しました';
+        userHash[socket.id] = name;
+        io.sockets.emit('publish', { value: msg });
+        console.log('connected');
     });
-    console.log('socket', req.testing);
-});
 
-app.listen(3000);
+    socket.on('publish', function (data) {
+        io.sockets.emit('publish', { value: data.value });
+    });
+
+    socket.on('disconnect', function () {
+        if (userHash[socket.id]) {
+            var msg = userHash[socket.id] + 'が退出しました';
+            delete userHash[socket.id];
+            io.sockets.emit('publish', { value: msg });
+        }
+    });
+});
